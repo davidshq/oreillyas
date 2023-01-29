bcbeb8b62f75c5667d2d850dbf39862bc0e0e030
Dave Mackey <dave@davemackey.net>
2023-01-29 13:48:01 -0500

    Update topics_payload to output a deduplicated list of topics as a file.

--- for-learning/topic_payloads.py
+++ for-learning/topic_payloads.py
@@ -1,25 +1,24 @@
+# Saves a list of deduplicated topics to a file
 import json
-import sqlite3
+import os.path
 
 fname = 'oreilly-pid.json'
 str_data = open(fname).read()
 data = json.loads(str_data)
-
-for book in data:
-    for topics_payload in book:
-        topics_payload = book.get('topics_payload', '')
-        print(topics_payload)
+all_topics = set()
 
 # Iterate through the topics_payload and parse out the uuid, slug, name, and score
-# and add them to a set
-topics = set()
+# and add them to a set, print the set to the display
 for book in data:
     for topics_payload in book:
-        topics_payload = book.get('topics_payload', '')
-        for topic_payload in topics_payload:
-            uuid = topic_payload[0]
-            slug = topic_payload[1]
-            name = topic_payload[2]
-            score = topic_payload[3]
-            topics.add((uuid, slug, name, score))
-print(topics)
+        topics = book.get('topics_payload', '')
+        for topic in topics:
+            uuid = topic.get('uuid', '')
+            slug = topic.get('slug', '')
+            name = topic.get('name', '')
+            score = topic.get('score', '')
+            all_topics.add((uuid, slug, name, score))
+
+file = 'all_topics.txt'
+with open(os.path.join(file), 'a+', encoding='utf-8', errors='replace') as outfile:
+    outfile.write(str(all_topics))
